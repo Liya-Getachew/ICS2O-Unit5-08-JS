093085047ff3cb35de5b50acdfc3fc72c02f69da
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2022-12-07 13:43:48 -0500

    pwa and dates

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2022 liya getachew All rights reserved
 //
 // Created by: liya getachew
-// Created on: Oct 2022
+// Created on: Dec 2022
 // This file contains the JS functions for index.html
 
 "use strict"
@@ -10,8 +10,8 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS2O-Unit5-08-JS/sw.js", {
+    scope: "/ICS2O-Unit5-08-JS/",
   })
 }
 
